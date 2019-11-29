b845befe6bfdaf8f6f623c3bfb3e1d10ecda57d6
user <abc>
2019-11-29 16:00:48 +0300

    done

---
+++ src/it/by/EvenOrOdd.java
@@ -0,0 +1,10 @@
+package it.by;
+
+//Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+        String result = (number % 2 == 0 ? "Even" : "Odd");
+        return result;
+    }
+}

ad0987074b4186c9b7769ab097605f1d75c9eca6
VladislavBY <vladislav.minsk.by@gmail.com>
2019-11-30 11:39:02 +0300

    done

--- src/it/by/EvenOrOdd.java
+++ src/it/by/EvenOrOdd.java
@@ -1,6 +1,6 @@
 package it.by;
 
-//Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+//Create a function  (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
 public class EvenOrOdd {
     public static String even_or_odd(int number) {
--- src/it/by/HighestAndLowest.java
+++ src/it/by/HighestAndLowest.java
@@ -17,7 +17,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class HighestAndLowest {
-
     public static String highAndLow(String numbers) {
         String re = "";
         numbers += " "; //fix ArrayIndexOutOfBoundsException

af8c40b8f8fddf3426857dc636f8d286c52abb1a
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2019-11-30 17:53:05 +0300

    done

---
+++ src/it/by/StringRepeat.java
@@ -0,0 +1,16 @@
+package it.by;
+
+//Write a function called repeatString which repeats the given String src exactly count times.
+//
+//        repeatStr(6, "I") // "IIIIII"
+//        repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+public class StringRepeat {
+    public static String repeatStr(final int repeat, final String string) {
+        String out = "";
+        for (int i = 0; i < repeat; i++) {
+            out += string;
+        }
+        return out;
+    }
+}

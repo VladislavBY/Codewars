f4bb2ee60cda4d25091dd7574f5c91801348b1e6
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2019-12-01 11:48:14 +0300

    done

---
+++ src/it/by/CenturyFromYear.java
@@ -0,0 +1,23 @@
+package it.by;
+
+//Introduction
+//        The first century spans from the year 1 up to and including the year 100, The second - from the year 101 up to and including the year 200, etc.
+//
+//        Task :
+//        Given a year, return the century it is in.
+//
+//        Input , Output Examples ::
+//        centuryFromYear(1705)  returns (18)
+//        centuryFromYear(1900)  returns (19)
+//        centuryFromYear(1601)  returns (17)
+//        centuryFromYear(2000)  returns (20)
+
+public class CenturyFromYear {
+    public static int century(int number) {
+        return number = (number % 100 == 0) ? number / 100 : number / 100 + 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(century(89));
+    }
+}

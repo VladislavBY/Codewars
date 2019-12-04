4d092ab4446f0c5536895e12bc8ae4259247a69e
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2019-12-04 11:53:41 +0300

    done

---
+++ src/it/by/DeodorantEvaporator.java
@@ -0,0 +1,26 @@
+package it.by;
+
+//This program tests the life of an evaporator containing a gas.
+//
+//        We know the content of the evaporator (content in ml), the percentage of foam or gas lost every day (evap_per_day) and the threshold (threshold) in percentage beyond which the evaporator is no longer useful. All numbers are strictly positive.
+//
+//        The program reports the nth day (as an integer) on which the evaporator will be out of use.
+
+public class DeodorantEvaporator {
+    public static int evaporator(double content, double evap_per_day, double threshold) {
+        int i;
+        double newContent = content;
+        for (i = 1; ; i++) {
+            newContent = newContent * ((100 - evap_per_day) / 100);
+            if (content / newContent >= threshold) {
+                break;
+            }
+        }
+
+        return i;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(evaporator(0, 10, 10));
+    }
+}

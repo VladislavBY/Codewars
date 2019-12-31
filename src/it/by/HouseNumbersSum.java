73fccff64b4f541cf56c8dd140c541b18b2ca9be
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2019-12-31 11:55:57 +0300

    done

---
+++ src/it/by/HouseNumbersSum.java
@@ -0,0 +1,12 @@
+package it.by;
+
+public class HouseNumbersSum {
+    public static int houseNumbersSum(final int[] arr) {
+        int outPut = 0;
+        for (int value : arr) {
+            if (value == 0) break;
+            else outPut += value;
+        }
+        return outPut;
+    }
+}

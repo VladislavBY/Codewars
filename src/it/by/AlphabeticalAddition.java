01c81142faa3b3867a4672ddcc03ee5ccb6f5162
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-01-01 12:19:27 +0300

    done

--- src/it/by/AlphabeticalAddition.java
+++ src/it/by/AlphabeticalAddition.java
@@ -4,8 +4,8 @@ public class AlphabeticalAddition {
     public static String addLetters(String... letters) {
         String abc = "zabcdefghijklmnopqrstuvwxyz";
         int sum = 0;
-        for (int i = 0; i < letters.length; i++) {
-            sum += abc.indexOf(letters[i]);
+        for (String letter : letters) {
+            sum += abc.indexOf(letter);
         }
         return String.valueOf(abc.charAt(sum % 26));
     }

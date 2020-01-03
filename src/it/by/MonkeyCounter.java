940fbb951429539ac2065a034ddf45da83d30a47
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-01-03 11:37:10 +0300

    done

---
+++ src/it/by/MonkeyCounter.java
@@ -0,0 +1,11 @@
+package it.by;
+
+public class MonkeyCounter {
+    public static int[] monkeyCount(final int n) {
+        int[] outPut = new int[n];
+        for (int i = 0; i < outPut.length; i++) {
+            outPut[i] = i + 1;
+        }
+        return outPut;
+    }
+}

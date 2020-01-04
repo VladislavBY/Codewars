3cb76f6bfeb1e5a9c0aabc1e67c0a4dd268e612f
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-01-04 13:32:19 +0300

    done

--- src/it/by/TheOldSwitcheroo.java
+++ src/it/by/TheOldSwitcheroo.java
@@ -1,4 +1,7 @@
 package it.by;
+// Kata.Vowel2Index("this is my string") == "th3s 6s my str15ng"
+// Kata.Vowel2Index("Codewars is the best site in the world") == "C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld"
+
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;

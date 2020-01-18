38a6b519a842e5176c0959f7ecfb072c5e655916
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-01-18 14:56:47 +0300

    done

---
+++ src/by/popkov/Maskify.java
@@ -0,0 +1,14 @@
+package by.popkov;
+
+public class Maskify {
+    public static String maskify(String str) {
+        if (str.length() > 4) {
+            return str.substring(0, str.length() - 4).replaceAll(".", "#") + str.substring(str.length() - 4);
+        }
+        return str;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maskify(""));
+    }
+}

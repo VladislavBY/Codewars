7a4cf501d88da00e1f383d5f740784e688af8d41
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-01-20 12:24:46 +0300

    done

--- src/by/popkov/EvenOrOdd.java
+++ src/by/popkov/EvenOrOdd.java
@@ -4,7 +4,6 @@ package by.popkov;
 
 public class EvenOrOdd {
     public static String even_or_odd(int number) {
-        String result = (number % 2 == 0 ? "Even" : "Odd");
-        return result;
+        return (number % 2 == 0 ? "Even" : "Odd");
     }
 }

e2cea5cede79c651b122a1bab41a873cbbd92fd0
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-03-05 16:07:34 +0300

    done

--- src/by/popkov/Clock.java
+++ src/by/popkov/Clock.java
@@ -1,6 +1,14 @@
 package by.popkov;
 
+
 public class Clock {
+    /**
+     *
+     * @param h hours
+     * @param m minuets
+     * @param s seconds
+     * @return returned sum h, m and s converted to milliseconds
+     */
     public static int Past(int h, int m, int s) {
         return h * 3600000 + m * 60000 + s * 1000;
     }

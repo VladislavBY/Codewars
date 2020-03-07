46d97f3124ad15b27ad69e77a7c808d85947bfd9
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-03-07 14:02:30 +0300

    done

---
+++ src/by/popkov/TwoToOne.java
@@ -0,0 +1,9 @@
+package by.popkov;
+
+public class TwoToOne {
+    public static String longest(String s1, String s2) {
+        StringBuilder stringBuilder = new StringBuilder();
+        (s1 + s2).chars().distinct().sorted().forEach(o -> stringBuilder.append((char) o));
+        return stringBuilder.toString();
+    }
+}

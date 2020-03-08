5619bc9dcbf070136e778b6380a6c86eba2a4cdd
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-03-08 14:56:07 +0300

    done

--- src/by/popkov/ConsonantValue.java
+++ src/by/popkov/ConsonantValue.java
@@ -1,8 +1,20 @@
 package by.popkov;
 
 public class ConsonantValue {
+    /**
+     * Given a lowercase string that has alphabetic characters only and no spaces,
+     * return the highest value of consonant substrings.
+     * Consonants are any letters of the alpahabet except "aeiou".
+     *
+     * @param s  the lowercase string
+     * @return the highest value of consonant substrings
+     */
     public static int solve(final String s) {
-
-        return 0;
+        int maxSum = 0;
+        for (String consonantSequence : s.split("[aeiou]+")) {
+            int sum = consonantSequence.chars().sum() - (consonantSequence.length() * 96);
+            if (sum > maxSum) maxSum = sum;
+        }
+        return maxSum;
     }
 }

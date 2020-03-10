add76fa6ee4036be7bc1c6eb623ab7030422a66c
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-03-10 11:37:05 +0300

    done

--- src/by/popkov/PersistentBugger.java
+++ src/by/popkov/PersistentBugger.java
@@ -13,7 +13,10 @@ public class PersistentBugger {
     public static int persistence(long n) {
         int counter = 0;
         for (long i = n; i > 9; counter++) {
-           i = String.valueOf(i).chars().map((o) -> o - 48).reduce((o1, o2) -> o1 * o2).orElse(0);
+           i = String.valueOf(i).chars()
+                   .map((o) -> o - 48)
+                   .reduce((o1, o2) -> o1 * o2)
+                   .orElse(0);
         }
         return counter;
     }

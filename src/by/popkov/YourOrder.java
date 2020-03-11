36644ee018e69acb8eada31dfdde73fe9c8f41be
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-03-11 13:41:02 +0300

    done

--- src/by/popkov/YourOrder.java
+++ src/by/popkov/YourOrder.java
@@ -6,6 +6,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class YourOrder {
+
     /**
      * Your task is to sort a given string. Each word in the string will contain a single number.
      * This number is the position the word should have in the result.

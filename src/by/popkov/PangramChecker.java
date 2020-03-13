81ff0f36857410795c13902aa2d9c353f1e258fa
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-03-13 17:39:40 +0300

    done

--- src/by/popkov/PangramChecker.java
+++ src/by/popkov/PangramChecker.java
@@ -1,6 +1,16 @@
 package by.popkov;
 
 public class PangramChecker {
+    /**
+     * A pangram is a sentence that contains every single letter of the alphabet at least once.
+     * For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram,
+     * because it uses the letters A-Z at least once (case is irrelevant).
+     *
+     * Given a string, detect whether or not it is a pangram.
+     * Return True if it is, False if not. Ignore numbers and punctuation.
+     * @param sentence the string for pangram check
+     * @return True if sentence is pangram, false if not
+     */
     public boolean check(String sentence) {
         return 26 == sentence.toLowerCase()
                 .replaceAll("[^a-z]", "")

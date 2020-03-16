f8f2684ccd6795f33b76b8f06c5355447a2eef86
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-03-16 16:33:44 +0300

    done

---
+++ src/by/popkov/java8/HumanReadableTime.java
@@ -0,0 +1,26 @@
+package by.popkov.java8;
+
+
+public class HumanReadableTime {
+    /**
+     * Write a function, which takes a non-negative integer (seconds)
+     * as input and returns the time in a human-readable format (HH:MM:SS)
+     * <p>
+     * HH = hours, padded to 2 digits, range: 00 - 99
+     * MM = minutes, padded to 2 digits, range: 00 - 59
+     * SS = seconds, padded to 2 digits, range: 00 - 59
+     * The maximum time never exceeds 359999 (99:59:59)
+     * <p>
+     * You can find some examples in the test fixtures.
+     *
+     * @param seconds the seconds for convert
+     * @return the time in HH:MM:SS format
+     */
+    public static String makeReadable(int seconds) {
+        return String.format("%02d:%02d:%02d", seconds / 3600, (seconds % 3600) / 60, (seconds % 3600) % 60);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(HumanReadableTime.makeReadable(359999));
+    }
+}

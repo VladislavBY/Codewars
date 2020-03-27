69ac8fa6142842528364300b2b53ff14b29e86c8
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-03-27 12:21:53 +0300

    done

--- src/by/popkov/java8/DuplicateEncoder.java
+++ src/by/popkov/java8/DuplicateEncoder.java
@@ -2,8 +2,6 @@ package by.popkov.java8;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class DuplicateEncoder {

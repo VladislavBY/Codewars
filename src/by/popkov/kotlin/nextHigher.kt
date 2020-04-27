9a3c8dfba75c3bcf1c92eb94d1732292cd1ac631
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-04-27 16:13:38 +0300

    done

---
+++ src/by/popkov/kotlin/nextHigher.kt
@@ -0,0 +1,16 @@
+package by.popkov.kotlin
+
+fun nextHigher(n: Int): Int {
+    var num = n
+    val count = num.toString(2).count { o: Char -> o == '1' }
+    while (true) {
+        num += 1
+        if (count == num.toString(2).count { o: Char -> o == '1' }) {
+            return num;
+        }
+    }
+}
+
+fun main() {
+    println(nextHigher(323423))
+}
